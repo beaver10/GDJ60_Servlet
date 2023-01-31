d10e1aee5ec1254e84b8c53d66db781cdaae2ae1
beaver10 <wkdgkdud1005@gmail.com>
2023-01-31 17:49:08 +0900

    1

--- src/main/java/com/iu/home/product/ProductService.java
+++ src/main/java/com/iu/home/product/ProductService.java
@@ -1,6 +1,49 @@
 package com.iu.home.product;
 
+import java.util.List;
+
 public class ProductService {
+	
+	
+	//실행순서 1 (잘안씀)
+	//멤버변수 선언 초기화
+	//결합도가 높다(강하다)
+	private ProductDAO productDAO = new ProductDAO();
+	
+	//실행순서 2 (잘안씀)
+	//초기화 블럭
+	{
+		this.productDAO = new ProductDAO();
+	}
+	
+	//실행순서 3
+	//생성자
+	public ProductService () {
+		this.productDAO = new ProductDAO();
+	}
+	
+	//실행순서 4
+	//setter
+	//결합도가 낮다(약하다)
+	public void setProductDAO(ProductDAO productDAO) {
+		this.productDAO = productDAO;
+	}
+	
+	
+	public int setAddProduct (ProductDTO productDTO, List<ProductOptionDTO>ar) throws Exception{
+		//product, option 등록
+		Long productNum = productDAO.getProductnum();
+		productDTO.setProductNum(productNum);
+		int result = productDAO.setAddProduct(productDTO);
+		for(ProductOptionDTO productOptionDTO:ar) {
+			productDTO.setProductNum(productNum);
+			result = productDAO.setAddProductOption(productOptionDTO);
+			
+		}
+		return result;
+		
+		
+	}
 
 	public static void main(String[] args) {
 		
